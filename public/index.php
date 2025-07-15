be0800aa022aa7e644c2cdd4558d0eb2d4cfa2b4
José Rocha <josemarlerocha@hotmail.com>
2025-07-14 22:16:50 -0300

    Adicionado os estilos e a tabela fake

--- public/index.php
+++ public/index.php
@@ -1,6 +1,28 @@
-<?php
-echo "Hello World test";
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets//css/styles.css">
+    <title>Home</title>
+</head>
+<body>
+    <div id="corpo">
+      <h1>Escolha Seu Jogo</h1>
 
-?>
+      <table class="listagem">
+        <tr>
+            <td>Foto</td>
+            <td>Nome</td>
+            <td>Adm</td>
+        </tr>
 
-<a href="teste.php">Go Teste teste </a>
+        <tr>
+            <td>Foto</td>
+            <td>Nome</td>
+            <td>Adm</td>
+        </tr>
+      </table>
+    </div>
+</body>
+</html>
