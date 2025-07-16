9e2e95f37a45711a27bd87a156c8b4b35be01033
José Rocha <josemarlerocha@hotmail.com>
2025-07-16 19:16:26 -0300

    Ajuste do titulo da página

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="stylesheet" href="./assets//css/styles.css">
-     <title>Homes</title>
+     <title>Home</title>
  </head>
 
  <body>
