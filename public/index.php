e0cf983c83f4fda85f8320ea942cf291a4e192b3
José Rocha <josemarlerocha@hotmail.com>
2025-07-20 17:16:47 -0300

    Adionado o segundo parâmetro nos links para ordenação dos items pesquisados

--- public/index.php
+++ public/index.php
@@ -10,6 +10,7 @@
     // var_dump($_ENV);
 
     // echo $ordenacao;
+    $busca = $_GET['busca'];
 ?>
 
  <!DOCTYPE html>
@@ -27,19 +28,23 @@
                  <div class="d-flex align-items-center col-12 col-lg-8 col-md-8 col-sm-12 col-xs-12">
                      <span> Ordenar por:
                          <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-                             href="/?ordenacao=nome">
+                             href="/?ordenacao=nome&busca=<?php echo $busca;?>">
+                             <!-- Adiconado o &busca=<?php echo $busca;?> 
+                                  para quando pesquisar por algo na busca e de pois clicar em um dos links
+                                  de ordenação ele fazer a ordenação certa do item pesquisado
+                            -->
                              Nome
                          </a> |
                          <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-                             href="/?ordenacao=produtora">
+                             href="/?ordenacao=produtora&busca=<?php echo $busca;?>">
                              Produtora
                          </a> | <a
                              class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-                             href="/?ordenacao=nota-alta">
+                             href="/?ordenacao=nota-alta&busca=<?php echo $busca;?>">
                              Nota Alta
                          </a> |
                          <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
-                             href="/?ordenacao=nota-baixa">
+                             href="/?ordenacao=nota-baixa&busca=<?php echo $busca;?>">
                              Nota Baixa
                          </a> |
                          <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
